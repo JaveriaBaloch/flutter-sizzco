1e3cbe843d9dce9399813783c3709f1d9a4fd035
Javeria Baloch <65242641+JaveriaBaloch@users.noreply.github.com>
2024-07-25 06:44:45 +0200

    Update Config.dart

--- lib/Config.dart
+++ lib/Config.dart
@@ -1,5 +1,5 @@
 class Config {
-  String ck = "ck_bcbafd3d7ab51cf72c0067b3a0d76d8c0bc8a11f";
-  String cs = "cs_4ead55310544ab4b642e467874d38bc397d20fa1";
+  String ck = customer_key;
+  String cs = customer_secret;
   String web = "https://sizzco.com";
 }
